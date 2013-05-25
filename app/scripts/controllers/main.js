2debe023b0bb09def177b6b8fba7baa3289c0dce
Peter Graham <peterghm@gmail.com>
2013-05-24 23:46:55 -0700

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('remixApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
